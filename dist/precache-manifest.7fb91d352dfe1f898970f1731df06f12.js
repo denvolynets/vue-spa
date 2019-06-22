9d403badb5ac01e7aaa87cabcd5f23c05c3e8b06
denysvol <info.denvol@gmail.com>
2019-06-22 15:34:01 +0300

    build dist
    Signed-off-by: denysvol <гinfo.denvol@gmail.com>

---
+++ dist/precache-manifest.7fb91d352dfe1f898970f1731df06f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  },
+  {
+    "revision": "d1452295225143fd697b",
+    "url": "/dist/js/chunk-vendors.7d8c8fa7.js"
+  },
+  {
+    "revision": "62bbe61315f922ff0ed0",
+    "url": "/dist/js/app.759ddd64.js"
+  },
+  {
+    "revision": "09aaeb72a7f50ecfd8d8d674bce9f9f0",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "d1452295225143fd697b",
+    "url": "/dist/css/chunk-vendors.cb759976.css"
+  },
+  {
+    "revision": "62bbe61315f922ff0ed0",
+    "url": "/dist/css/app.b50fc0a3.css"
+  }
+];
